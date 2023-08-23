4c458dd9b86d1a3ac1fd8bb35c995bf975a0e262
Laura <Laura@DESKTOP-BESSB6E>
2023-08-23 21:39:57 +0300

    Add project files.

---
+++ Uzduotis1_IF/Program.cs
@@ -0,0 +1,13 @@
+﻿int number = Convert.ToInt32(Console.ReadLine());
+if (number > 100)
+{
+    Console.WriteLine("Skaicius yra didesnis nei 100 ");
+}
+else if (number < 100)
+{
+    Console.WriteLine("Skaicius yra mazesnis nei 100");
+}
+else
+{
+    Console.WriteLine("Skaicius yra lygus 100 ");
+}
