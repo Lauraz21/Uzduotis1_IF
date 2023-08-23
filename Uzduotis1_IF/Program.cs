cae268ccfdd414653dd423d1510f3f44e9a9532a
Laura <Laura@DESKTOP-BESSB6E>
2023-08-23 22:40:57 +0300

    added Input Text

--- Uzduotis1_IF/Program.cs
+++ Uzduotis1_IF/Program.cs
@@ -1,7 +1,8 @@
-﻿int number = Convert.ToInt32(Console.ReadLine());
+﻿Console.Write("iveskite skaiciu: ");
+int number = Convert.ToInt32(Console.ReadLine());
 if (number > 100)
 {
-    Console.WriteLine("Skaicius yra didesnis nei 100 ");
+    Console.WriteLine("Skaicius yra didesnis nei 100");
 }
 else if (number < 100)
 {
@@ -9,5 +10,5 @@ else if (number < 100)
 }
 else
 {
-    Console.WriteLine("Skaicius yra lygus 100 ");
+    Console.WriteLine("Skaicius yra lygus 100");
 }
